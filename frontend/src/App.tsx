4cc986cd79ff0787f501c617d52d29f9229adf3d
Ryantayy <Ryan.TayPengYeow@lincolnuni.ac.nz>
2024-07-04 15:26:32 +1200

    Update

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div>
+      {/* Navbar */}
+      {/* < Navbar /> */}
+
+      {/* Wrapper */}
+      <div className="wrapper">
+        {/* //Routes// */}
+      </div>
+    </div>
+  )
+}
+
+export default App
